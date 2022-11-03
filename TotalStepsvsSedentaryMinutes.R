3d827e7c88c12d18860638a04f3e1a98eca7ae27
sriharinirm <111257527+sriharinirm@users.noreply.github.com>
2022-11-02 23:27:34 -0400

    Update TotalStepsvsSedentaryMinutes.R

--- TotalStepsvsSedentaryMinutes.R
+++ TotalStepsvsSedentaryMinutes.R
@@ -1,7 +1,10 @@
 ``{r}
 library(tidyverse)
 library(ggplot2)
+
 daily_activity <- read_csv("https://raw.githubusercontent.com/sriharinirm/bios-611-project/main/source_data/dailyActivity_merged.csv")
-ggplot(data=daily_activity, aes(x=TotalSteps, y=SedentaryMinutes)) + geom_point()
+fig = ggplot(data=daily_activity, aes(x=TotalSteps, y=SedentaryMinutes)) + geom_point()
+
+ggsave("figures/TotalStepsvsSedentaryMinutes.png", figure = fig)
 
 ```
