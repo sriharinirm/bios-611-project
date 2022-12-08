87af23a7e3446d4947fc825e4ae44701ae7d6e70
Sriharini Radhakrishnan Madanraj <111257527+sriharinirm@users.noreply.github.com>
2022-12-08 15:28:16 -0500

    Update CorrelationFigures.R

--- scripts/CorrelationFigures.R
+++ scripts/CorrelationFigures.R
@@ -3,5 +3,7 @@ library(lubridate)
 library(corrplot)
 
 CorrelationFigure1 <- plot(select(correlation_data, Calories, TotalMinutesAsleep, TotalSteps))
+ggsave("CorrelationFigure1")
 
 CorrelationFigure2 <- plot(select(correlation_data, TotalDistance, TotalSteps))
+ggsave("CorrelationFigure2")
