69deb3598c10cd360ed33dc323b8740aa5e0f8ab
Sriharini Radhakrishnan Madanraj <111257527+sriharinirm@users.noreply.github.com>
2022-12-08 12:32:52 -0500

    Update CorrelationFigures.R

--- scripts/CorrelationFigures.R
+++ scripts/CorrelationFigures.R
@@ -2,6 +2,6 @@ library(tidyverse)
 library(lubridate)
 library(corrplot)
 
-plot(select(correlation_data, Calories, TotalMinutesAsleep, TotalSteps))
+CorrelationFigure1 <- plot(select(correlation_data, Calories, TotalMinutesAsleep, TotalSteps))
 
-plot(select(correlation_data, TotalDistance, TotalSteps))
+CorrelationFigure2 <- plot(select(correlation_data, TotalDistance, TotalSteps))
