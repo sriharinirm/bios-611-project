c5436411627be3659284a7ff2af415f235a0d55d
Sriharini Radhakrishnan Madanraj <111257527+sriharinirm@users.noreply.github.com>
2022-12-08 12:16:02 -0500

    Delete TotalStepsvsSedentaryMinutes.R

--- TotalStepsvsSedentaryMinutes.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(ggplot2)
-
-daily_activity <- read_csv("source_data/dailyActivity_merged.csv")
-fig = ggplot(data=daily_activity, aes(x=TotalSteps, y=SedentaryMinutes)) + geom_point()
-
-ggsave("figures/TotalStepsvsSedentaryMinutes.png", figure = fig)
-
-
