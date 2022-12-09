6466c61651021f1920b39a1d5483d5f7b476b4a7
Sriharini Radhakrishnan Madanraj <111257527+sriharinirm@users.noreply.github.com>
2022-12-08 23:08:19 -0500

    Create AverageSleepPlot.R

---
+++ scripts/AverageSleepPlot.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(lubridate)
+library(corrplot)
+
+hourly_sleep <- read.csv("daily_activity_and_sleep.csv")
+
+avg_dayofweek_sleep <- daily_activity_and_sleep %>% 
+group_by(DayOfWeek) %>% 
+summarise_at(c(AvgDistance = "TotalDistance", AvgSleepMinutes = "TotalMinutesAsleep"), mean,na.rm = TRUE)
+
+AverageSleepPlot <- ggplot(data = avg_dayofweek_sleep, aes(x= DayOfWeek, y = AvgSleepMinutes, fill = AvgSleepMinutes)) + 
+    geom_bar(stat="identity") + 
+    scale_fill_gradient (low="orange", high= "brown")
+ggsave("AverageSleepPlot")
