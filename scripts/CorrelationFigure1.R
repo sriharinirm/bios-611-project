a69f327ebea3e0a5fe34501f4f5b86de34ea7de2
Sriharini Radhakrishnan Madanraj <111257527+sriharinirm@users.noreply.github.com>
2022-12-08 23:02:14 -0500

    Update and rename CorrelationFigures.R to CorrelationFigure1.R

--- scripts/CorrelationFigure1.R
+++ scripts/CorrelationFigure1.R
@@ -4,6 +4,3 @@ library(corrplot)
 
 CorrelationFigure1 <- plot(select(correlation_data, Calories, TotalMinutesAsleep, TotalSteps))
 ggsave("CorrelationFigure1")
-
-CorrelationFigure2 <- plot(select(correlation_data, TotalDistance, TotalSteps))
-ggsave("CorrelationFigure2")
