50988f3504cc3b80e6e414bed661033da15deb98
Sriharini Radhakrishnan Madanraj <111257527+sriharinirm@users.noreply.github.com>
2022-12-08 22:16:24 -0500

    Update and rename StepsHeatmap.R to StepsAnalysis.R

--- scripts/StepsAnalysis.R
+++ scripts/StepsAnalysis.R
@@ -4,16 +4,12 @@ library(corrplot)
 
 hourly_steps <- read.csv("hourlySteps_merged.csv")
 
-head(hourly_steps)
-
 hourly_steps <- hourly_steps %>% mutate(ActivityHour = mdy_hms(ActivityHour))
 
 hourly_steps <- hourly_steps %>% mutate(hourly_steps, DayOfWeek=weekdays(ActivityHour)) %>%
     mutate(hourly_steps,TimeOfDay= format(ActivityHour, format = "%H"))
 
 hourly_steps <- mutate(hourly_steps, DayOfWeek= factor(DayOfWeek, levels = c("Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")))
-
-head(hourly_steps)
 
 avg_hourly_steps_as_per_time_of_day <- hourly_steps %>% 
 group_by(DayOfWeek, TimeOfDay) %>% 
@@ -28,4 +24,7 @@ avg_hourly_steps_as_per_day_of_week <-avg_hourly_steps_as_per_time_of_day %>%
 group_by(DayOfWeek) %>%
 summarise(StepsPerDay = sum(AvgSteps))
 
-avg_hourly_steps_as_per_day_of_week
+AverageStepsPlot <- ggplot(data = avg_hourly_steps_as_per_day_of_week, aes(x= DayOfWeek, y = StepsPerDay, fill = StepsPerDay)) + 
+    geom_bar(stat="identity") + 
+    scale_fill_gradient (low="pink", high= "red")
+ggsave("AverageStepsPlot")
